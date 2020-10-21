2cff844fdbf96abcfe75a6b5183ec31199f575c1
alexmasturkov <mastrukov@yahoo.ca>
2020-10-20 20:29:43 -0400

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import {HomeComponent} from './pages/home/home.component';
+import {ContactComponent} from './pages/contact/contact.component';
+
+const routes: Routes = [
+
+  {
+    path:'contact',
+    component:ContactComponent,
+    pathMatch:'full',
+   
+    },
+    {
+      path:'home',
+      pathMatch:'full',
+      component:HomeComponent,
+      } 
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
